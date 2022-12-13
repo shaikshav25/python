b70613562d56199d4f789806babe60ded7f6640e
shaikshav25 <120363283+shaikshav25@users.noreply.github.com>
2022-12-13 14:34:21 +0530

    Create prog1pdf2.py

---
+++ prog1pdf2.py
@@ -0,0 +1,9 @@
+format
+print("""Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are""")
